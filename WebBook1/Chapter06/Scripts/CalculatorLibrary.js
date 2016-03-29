fdbf210fecdf03b8b1325e58c954e82edc066dad
Steve New <stevoe@gmail.com>
2016-03-29 20:13:47 +1100

    Fixed up errors found by using JSHint (for the first time). Also added (empty) JSHint config file

--- WebBook1/Chapter06/Scripts/CalculatorLibrary.js
+++ WebBook1/Chapter06/Scripts/CalculatorLibrary.js
@@ -13,7 +13,7 @@
       $('#btnClear').on('click', calculator.clear);
 
       calculator.clear();
-  }
+  };
 
   ns.Calculator = (function() {
 
@@ -22,26 +22,26 @@
 
     Calculator.prototype.numberClick = function () {
         $('#txtInput').val($('#txtInput').val() == '0' ? this.innerText : $('#txtInput').val() + this.innerText);
-    }
+    };
 
     Calculator.prototype.plusClick = function () {
         $('#txtResult').val(Number($('#txtResult').val()) + Number($('#txtInput').val()));
         Calculator.prototype.clearEntry();
-    }
+    };
 
     Calculator.prototype.minusClick = function () {
         $('#txtResult').val(Number($('#txtResult').val()) - Number($('#txtInput').val()));
         Calculator.prototype.clearEntry();
-    }
+    };
 
     Calculator.prototype.clearEntry = function () {
         $('#txtInput').val('0');
-    }
+    };
 
     Calculator.prototype.clear = function () {
         $('#txtInput').val('0');
         $('#txtResult').val('0');
-    }
+    };
 
     return Calculator;
   })();
