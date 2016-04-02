1dbbf22ca525dc62aca2f251f621c7f0cf1d6efd
Steve New <stevoe@gmail.com>
2016-04-02 21:22:18 +1100

    CH08 Ex1 & Ex2 1. Ex1 - Node web server that returns a HTML response to client sending contact info 2. Ex2 - JSON response to client sending contact info, and display on page

---
+++ WebBook1/Chapter08/ContosoWeb/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var formidable = require('formidable');
+
+app.use(express.static(__dirname + '/WebCalculatorSolution/WebCalculator'));
+app.get('/', function(request, response) {
+  response.redirect('default.html');
+});
+
+app.post('/ContactMessage', function(request, response) {
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(err, fields) {
+    var lastName = fields.lastName,
+        firstName = fields.firstName,
+        email = fields.email,
+        message = fields.message;
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('Thankyou, ' + firstName + ' ' + lastName + '<br />');
+    response.write('We will contact you at ' + email + '<br />');
+    response.end('Your message: ' + message + '<br />');
+    console.log('Handled message for ' + firstName + ' ' + lastName);
+  });
+});
+
+app.post('/ContactService', function(request, response) {
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(err, fields) {
+    var lastName = fields.lastName,
+        firstName = fields.firstName,
+        email = fields.email,
+        message = fields.message,
+        result = 'Thankyou, ' + firstName + ' ' + lastName + '<br />'
+                + 'We will contact you at ' + email + '<br />'
+                + 'Your message: ' + message + '<br />';
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end('{ "result": "' + result + '" }');
+    console.log('Handled message for ' + firstName + ' ' + lastName);
+  });
+});
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port: ' + port);
