c52a853e9605e6bcfc589e88c2a91edf5e4d3a02
Steve New <stevoe@gmail.com>
2016-04-28 23:36:17 +1000

    Set up "default.html" page containing HTML for Maths-Stage1-WholeNumbers1

---
+++ 01_syllabus/NodeServer/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app=express();
+
+//var jade = require('jade');
+
+app.use(express.static(__dirname + '/public'));
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port: ' + port);
