c47b1b72cea62d469f3cf78c6510794b65f8d18a
Steve New <stevoe@gmail.com>
2016-05-16 20:28:07 +1000

    Added template node application and raw text of first book

---
+++ 02_bookreader/NodeServer/app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app=express();
+
+app.use(express.static(__dirname + '/public'));
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port: ' + port);
